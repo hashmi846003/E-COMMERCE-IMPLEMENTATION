a27cf7410e2712b207866bd4a0b033a6a137c5f1
Fakhruddin Hashmi <109304853+hashmi846003@users.noreply.github.com>
2025-07-23 14:04:45 +0530

    Update handler.go

---
+++ consumer/handler.go
@@ -0,0 +1,11 @@
+package consumer
+
+import "github.com/gin-gonic/gin"
+
+func Home() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Welcome Consumer 🛒",
+		})
+	}
+}
